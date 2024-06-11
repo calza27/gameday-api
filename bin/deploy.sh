501c8b207251a54edb2f1d9e755a3f7b5ab5edcc
calza27 <calza27@gmail.com>
2024-06-11 16:51:35 +1000

    Update deploy script to build golang that doesn't exist yet

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,6 +3,7 @@ die() { echo "${1:-ouch}" >&2; exit "${2:-1}"; }
 
 hash git  2>/dev/null || die "missing dep: git"
 hash sam  2>/dev/null || die "missing dep: sam"
+hash aws  2>/dev/null || die "missing dep: aws"
 
 API_NAME="GameDay-api"
 STACK_NAME="GameDay-api"
@@ -12,6 +13,17 @@ echo "~~~ :git: Get GIT info"
 git_hash=$(git rev-parse HEAD)
 git_repo=$(git config --get remote.origin.url)
 tags="git:hash=${git_hash} git:branch=${GIT_BRANCH} ops:origin=${git_repo} ops:name=${API_NAME}"
+
+echo "~~~ :aws: Build code using iterative golang compiling"
+for path in ./cmd/*/; do
+  dirname=$(basename "$path")
+
+  echo "Building $dirname..."
+
+  cd "./cmd/$dirname" || die "failed to cd into ./cmd/$dirname"
+  GOOS=linux GOARCH=amd64 go build -tags lambda.norpc -o "../bin/$dirname/bootstrap" "$dirname.go"
+  cd ../..
+done
 
 echo "~~~ :aws: Deploy SAM Stack for branch $GIT_BRANCH"
 sam deploy \
