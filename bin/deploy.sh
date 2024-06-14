79e1aba3a7359fe5d691b633c89089ddd7e67be8
calza27 <calza27@gmail.com>
2024-06-14 22:00:54 +1000

    Uncomment deploy steps

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -17,25 +17,25 @@ STACK_NAME="GameDay-api"
 tags=$(./bin/parse-yaml.sh ./cf/tags.yaml) || die "failed to parse tags"
 bucket_name=$(aws ssm get-parameter --profile "$profile" --name /s3/cfn-bucket/name --query "Parameter.Value" --output text) || die "failed to get name of cfn bucket"
 
-# echo "~~~ Build code using iterative golang compiling"
-# for path in ./cmd/*/; do
-#   dirname=$(basename "$path")
+echo "~~~ Build code using iterative golang compiling"
+for path in ./cmd/*/; do
+  dirname=$(basename "$path")
 
-#   echo "Building $dirname..."
+  echo "Building $dirname..."
 
-#   cd "./cmd/$dirname" || die "failed to cd into ./cmd/$dirname"
-#   GOOS=linux GOARCH=amd64 go build -tags lambda.norpc -o "../bin/$dirname/bootstrap" "$dirname.go"
-#   cd ../..
-# done
+  cd "./cmd/$dirname" || die "failed to cd into ./cmd/$dirname"
+  GOOS=linux GOARCH=amd64 go build -tags lambda.norpc -o "../bin/$dirname/bootstrap" "$dirname.go"
+  cd ../..
+done
 
-# echo "~~~ Deploy processor stack"
-# sam deploy \
-#   --tags "$tags" \
-#   --no-fail-on-empty-changeset \
-#   --s3-bucket "${bucket_name}" \
-#   --stack-name "${STACK_NAME}-processing" \
-#   --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
-#   --profile "${profile}" || die "failed to deploy stack "$STACK_NAME"-processing"
+echo "~~~ Deploy processor stack"
+sam deploy \
+  --tags "$tags" \
+  --no-fail-on-empty-changeset \
+  --s3-bucket "${bucket_name}" \
+  --stack-name "${STACK_NAME}-processing" \
+  --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
+  --profile "${profile}" || die "failed to deploy stack "$STACK_NAME"-processing"
 
 echo "~~~ Deploy gateway stack"
 sam deploy \
