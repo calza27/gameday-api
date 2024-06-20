91e78b578c1e7a88f65b7fc49e70ebc3046c4911
calza27 <calza27@gmail.com>
2024-06-20 13:59:10 +1000

    remove comments

--- internal/filebuilder/csv_builder.go
+++ internal/filebuilder/csv_builder.go
@@ -14,7 +14,6 @@ func BuildCsv(gameData models.GameData) ([]byte, error) {
 	w := csv.NewWriter(byteWriter)
 
 	//player table
-	//id,surname,given_name,number
 	var data [][]string
 	data = append(data, []string{"id", "Surname", "Given", "Number"})
 	for _, player := range gameData.TeamAPlayers {
@@ -26,7 +25,6 @@ func BuildCsv(gameData models.GameData) ([]byte, error) {
 	data = [][]string{}
 
 	//gate data
-	//id, gameDate, competition, home, homeAbbr, venue, opposition, oppAbbr, level, round, launchArray
 	data = append(data, []string{"id", "gameDate", "competition", "home", "homeAbbr", "venue", "opposition", "oppAbbr", "level", "round"})
 	data = append(data, []string{gameData.Id, gameData.GameDate, gameData.Competition, gameData.TeamA, gameData.TeamAAbbr, gameData.Venue, gameData.TeamB, gameData.TeamBAbbr, gameData.Level, gameData.Round, "[]"})
 	data = append(data, []string{string('\u200B')})
@@ -35,7 +33,6 @@ func BuildCsv(gameData models.GameData) ([]byte, error) {
 	data = [][]string{}
 
 	//scoring table
-	//id,quarter,team,score_event,goal_scorer,score_type,hc_worm,launcher_number,type_number,op_worm
 	data = append(data, []string{"id", "quarter", "team", "ScoreEvent", "goalScorer", "scoreType", "HCWorm", "LauncherNo", "typeNumber", "OpWorm"})
 	for _, scoringEvent := range gameData.ScoringEvents {
 		data = append(data, []string{scoringEvent.Id, fmt.Sprintf("%d", scoringEvent.Quarter), scoringEvent.Team, scoringEvent.ScoreEvent, scoringEvent.GoalScorer, scoringEvent.ScoreType, fmt.Sprintf("%d", scoringEvent.HCWorm), fmt.Sprintf("%d", scoringEvent.LauncherNumber), fmt.Sprintf("%d", scoringEvent.TypeNumber), fmt.Sprintf("%d", scoringEvent.OpWorm)})
@@ -46,7 +43,6 @@ func BuildCsv(gameData models.GameData) ([]byte, error) {
 	data = [][]string{}
 
 	//quarter time table
-	//id,quarter,time
 	data = append(data, []string{"id", "quarter", "time"})
 	for _, quarterTime := range gameData.QuarterTimes {
 		data = append(data, []string{quarterTime.Id, fmt.Sprintf("%d", quarterTime.Quarter), quarterTime.Time})
@@ -57,7 +53,6 @@ func BuildCsv(gameData models.GameData) ([]byte, error) {
 	data = [][]string{}
 
 	//app storage table
-	//appStorage,data
 	data = append(data, []string{"appStorage", "data"})
 	for _, appStorageEvent := range gameData.AppStorage {
 		d := []string{appStorageEvent.DataType}
